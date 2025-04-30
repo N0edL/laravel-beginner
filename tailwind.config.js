95b8c2b9b0b7b46808dd8f4bfb922e8bca34c48e
NoedL <88205753+N0edL@users.noreply.github.com>
2025-04-30 15:02:05 +0200

    Refactor Tailwind CSS configuration: add extended color palette to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,24 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                'black': "#000000",
+                'dark-gray': "#1A1A1A",
+                'gray': "#262626",
+                'light-gray': "#3A3A3C",
+                'blue': "#0A84FF",
+                'indigo': "#5E5CE6",
+                'purple': "#BF5AF2",
+                'mint': "#66D4CF",
+                'green': "#30D158",
+                'yellow': "#FFD60A",
+                'orange': "#FF9F0A",
+                'pink': "#FF375F",
+                'red': "#FF453A",
+                'white': "#FFFFFF",
+                'light-white': "#F2F2F7",
+            },
+
             fontFamily: {
                 sans: [
                     "-apple-system",
